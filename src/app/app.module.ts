5d4f1fefafd40dc44b8540bc39e524c17817bd44
Satyadeep Singh <deep@idworks.com>
2018-09-05 17:04:07 -0500

    npm update mess, works now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ export const createTranslateLoader = (http: HttpClient) => {
     /* for development
     return new TranslateHttpLoader(
         http,
-        '/start-angular/SB-Admin-BS4-Angular-6/master/dist/assets/i18n/',
+        '/start-angular/Quick-Select-Angular-Master/master/dist/assets/i18n/',
         '.json'
     ); */
     return new TranslateHttpLoader(http, './assets/i18n/', '.json');
