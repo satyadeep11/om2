1fb6f6212a0ebd2e49dce85d055bc95fd2c0f042
Satyadeep Singh <deep@idworks.com>
2018-10-01 11:16:23 -0500

    added companion products inside product detail

--- src/app/customer-layout/brandfilter.pipe.ts
+++ src/app/customer-layout/brandfilter.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'brandfilter',
-  pure: false
+  pure: false//important when passing array to pipes
 })
 export class BrandFilterPipe implements PipeTransform {
   transform(items: any[], searcharr:any[]): any[] {
