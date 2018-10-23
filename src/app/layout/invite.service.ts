acf026c91a8a65c51b7cf866a3d0e2229aa8e178
Satyadeep Singh <deep@idworks.com>
2018-10-23 14:51:11 -0500

    added product security listing in invite module and it saves the selection in db

--- src/app/layout/invite.service.ts
+++ src/app/layout/invite.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable} from 'rxjs';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
-import { InviteUser} from './invite/invite.component';
+import { InviteUser,SecurityGroups} from './invite/invite.component';
 import { map } from 'rxjs/operators';
 import {GlobalCart} from '../customer-layout/globalcart';
 
@@ -21,6 +21,18 @@ export class InviteService {
     return this._http.post(
       link,
       person,
+      options
+    ).pipe(map((res: Response) => res.json()));
+  }
+
+  getSecurityGroups(): Observable<SecurityGroups>{
+ 
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    let link=this.gc.link+"/php_api/masterdb/get-security-groups.php";    
+  
+    return this._http.post(
+      link,
       options
     ).pipe(map((res: Response) => res.json()));
   }
--- src/app/layout/invite/invite.component.ts
+++ src/app/layout/invite/invite.component.ts
@@ -10,18 +10,32 @@ import { InviteService } from '../invite.service';
   animations: [routerTransition()]
 })
 export class InviteComponent implements OnInit {
+  myData:any;
+  public closed = false;  
+  email: any;
+  securityGroups:any;
+  sgList=[];
+  public sgListerror = false;  
 
   constructor(public router: Router,
-              private inviteService: InviteService) { }
-      myData:any;
-      public closed = false;  
-      email: any;
+              private inviteService: InviteService) { 
+                  this.inviteService.getSecurityGroups().subscribe(user => {
+                    console.log(user);  
+                    this.securityGroups=user;                 
+                      },
+                    error => console.log(error)
+                  );
+              }
+      
 
   ngOnInit() {}
 
   userInvite(person: InviteUser){
-   this.closed = false; 
-   person.inviteid=localStorage.getItem("uuid").toString(); 
+   
+   this.sgListerror =false;
+   if(this.sgList.join()){
+   person.inviteid=localStorage.getItem("uuid").toString();
+   person.sglist=this.sgList.join();
 
     this.inviteService.invite(person)
     .subscribe(user => {
@@ -30,6 +44,7 @@ export class InviteComponent implements OnInit {
 
       // go back to list of products
       this.myData = user; 
+      this.closed = false; 
       // if(this.myData.error){
       //   this.router.navigateByUrl('/error');
       // }
@@ -39,9 +54,25 @@ export class InviteComponent implements OnInit {
       //   localStorage.setItem('lname', this.myData.user['Last Name']);
       //   this.router.navigateByUrl('/dashboard');
       // }     
-   },
-   error => console.log(error)
-  );
+    },
+    error => console.log(error)
+    );
+    }
+    else{
+      this.sgListerror =true;
+    }
+  }
+
+  AddRemove(groupID,event) {//event gets the checkbox checked or not value
+    
+  
+    if ( event.target.checked ) {
+      this.sgList.push(groupID);
+    } 
+    else {
+      this.sgList=this.sgList.filter(item => item !== groupID);
+    }
+    console.log(this.sgList.join());
   }
 
 }
@@ -49,4 +80,12 @@ export class InviteComponent implements OnInit {
 export interface InviteUser {  
   email: string;
   inviteid: string;
+  sglist:string;
 }
+
+export interface SecurityGroups {  
+  groupId:number;
+  groupName: string;
+}
+
+
